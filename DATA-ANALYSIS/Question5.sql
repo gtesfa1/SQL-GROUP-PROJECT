4112c21a4ee3686b4377bbc3d8dadbe71b3aec75
MikiastesfayeJ <Mikiastesfaye998@gmail.com>
2023-06-05 02:29:11 +0300

    finished data exploration #6 and #7

---
+++ DATA-ANALYSIS/Question5.sql
@@ -0,0 +1,4 @@
+--List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+select first_name, last_name,sex
+from public.employees
+where first_name = 'Hercules' and last_name like 'B%'
